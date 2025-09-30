1efefc49c82705d9fdce3d19efe3497a18a67a7a
Jack A T Dalton <jackdalton83@gmail.com>
2025-09-30 12:41:40 +0100

    many changes

--- Miniprep/Miniprep24.py
+++ Miniprep/Miniprep24.py
@@ -1,7 +1,7 @@
 from opentrons import protocol_api
 metadata = {
     'protocolName': 'Vacuum miniprep 24 samples',
-    'description': 'Miniprep 24 samples using 3D printed vacuum manifold. Start with spun down cells in a deep 24 well plate.',
+    'description': 'Miniprep 24 samples using 3D printed vacuum manifold. Start with resuspended cells in a deep 24 well plate.',
     'author': 'JATD'
     }
 
@@ -13,12 +13,12 @@ requirements = {
 def run(protocol: protocol_api.ProtocolContext):
     # Load labware
     tips1 = protocol.load_labware("opentrons_flex_96_tiprack_50ul", location = "A2") 
-    #tips2 = protocol.load_labware("opentrons_flex_96_tiprack_50ul", location = "B1")
+    tips2 = protocol.load_labware("opentrons_flex_96_tiprack_50ul", location = "C1")
     trash = protocol.load_trash_bin(location="A3")
     left_pipette = protocol.load_instrument(
         "flex_1channel_50",
         mount="left",
-        tip_racks=[tips1]
+        tip_racks=[tips1,tips2]
     )
 
     # define reagents and labware 
@@ -27,33 +27,36 @@ def run(protocol: protocol_api.ProtocolContext):
     buffers = protocol.load_labware('jd_6_falconrack_50ml', 'C2')
     elution_tubes = protocol.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', 'C3')
 
-    ### 1. Lyse cells by resuspending in 250ul Lysis buffer ### 
-    for i in range(24):
+    ### 1. Lyse cells by adding 250ul Lysis buffer to resuspended cells ### 
+    # this block adds lysis buffer to each well at the top
+    left_pipette.pick_up_tip()
+    for i in range(24):  
         left_pipette.transfer(
             250,
             buffers["A1"], 
-            cells.wells()[i],
-            new_tip="once",
-            mix_after=(5, 45)
+            cells.wells()[i].top(0),
+            new_tip="never",
         )
-        left_pipette.transfer(
-            250,
-            buffers["A2"], 
-            cells.wells()[i],
-            new_tip="once",
-            mix_after=(5, 45)
-        )
+    left_pipette.drop_tip()
+    # this block mixes each well after adding lysis buffer getting  new tip each time
+    for i in range(24):
+        left_pipette.pick_up_tip()
+        left_pipette.mix(3,45, cells.wells()[i])
+        left_pipette.drop_tip()
 
+    protocol.comment("Neutralising samples")
 
     ### 2. Neutralize by adding 350ul Neutralisation buffer ###
     for i in range(24):
+        left_pipette.pick_up_tip()
         left_pipette.transfer(
             350,
             buffers["A3"], 
-            cells.wells()[i],
-            new_tip="once",
-            mix_after=(5, 45)
+            cells.wells()[i].top(0),
+            new_tip="never"
         )
+        left_pipette.mix(3,45, cells.wells()[i])
+        left_pipette.drop_tip()
 
     ### Pause for user to spin down samples and load into vacuum manifold ###
     protocol.pause("Please spin down samples and load into vacuum manifold. Click Resume when ready.")
@@ -64,14 +67,14 @@ def run(protocol: protocol_api.ProtocolContext):
         left_pipette.transfer(
             500,
             buffers["B1"], 
-            cells.wells()[i].top(3),
+            cells.wells()[i].top(1),
             new_tip="never",
         )
     for i in range(24):
         left_pipette.transfer(
             500,
             buffers["A3"], 
-            cells.wells()[i].top(3),
+            cells.wells()[i].top(1),
             new_tip="never",
         )
     left_pipette.drop_tip()
@@ -80,10 +83,10 @@ def run(protocol: protocol_api.ProtocolContext):
     left_pipette.distribute(
             50,
             buffers["B2"], 
-            VacManifold.wells(),
+            elution_tubes.wells(),
         )
     # End of protocol
-    protocol.comment("Protocol complete")
+    protocol.comment("Protocol complete, spind down samples and store elutions at -20C")
 
 
 
